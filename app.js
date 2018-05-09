6db3faad9992c723aeea9e96e22e3a2b1f843465
Alex Strand <ajstrand8@gmail.com>
2018-05-08 22:37:27 -0400

    removed unused files

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var helmet = require('helmet');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var redditRequests = require('./routes/redditRequests');
 
 
--- public/bundle.js
+++ public/bundle.js
@@ -32675,6 +32675,8 @@
 	
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 	
+	var Loading = __webpack_require__(192);
+	
 	var Image = function (_React$Component) {
 	  _inherits(Image, _React$Component);
 	
@@ -32691,6 +32693,9 @@
 	  }
 	
 	  _createClass(Image, [{
+	    key: "componentWillMount",
+	    value: function componentWillMount() {}
+	  }, {
 	    key: "render",
 	    value: function render() {
 	      return _react2.default.createElement(
